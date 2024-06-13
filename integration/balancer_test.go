dc3c3930e02d925b75d92fca66cf532757ced897
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-06-13 04:51:54 +0300

    feat(tests): implement integration test for load balancer

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -2,6 +2,8 @@ package integration
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"net/http"
 	"os"
 	"testing"
@@ -19,14 +21,34 @@ func TestBalancer(t *testing.T) {
 		t.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
-	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+	response1, err := client.Get(fmt.Sprintf("%s/api/v1/some-data2", baseAddress))
+	require.NoError(t, err)
+	response1Header := response1.Header.Get("lb-from")
+	assert.Equal(t, "server1:8080", response1Header)
+
+	response2, err := client.Get(fmt.Sprintf("%s/api/v1/some-data5", baseAddress))
+	require.NoError(t, err)
+	response2Header := response2.Header.Get("lb-from")
+	assert.Equal(t, "server2:8080", response2Header)
+
+	response3, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+	require.NoError(t, err)
+	response3Header := response3.Header.Get("lb-from")
+	assert.Equal(t, "server3:8080", response3Header)
+
+	response1Repeat, err := client.Get(fmt.Sprintf("%s/api/v1/some-data2", baseAddress))
+	require.NoError(t, err)
+	response1RepeatHeader := response1Repeat.Header.Get("lb-from")
+	assert.Equal(t, "server1:8080", response1RepeatHeader)
 }
 
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		b.Skip("Integration test is not enabled")
+	}
+
+	for i := 0; i < b.N; i++ {
+		_, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		require.NoError(b, err)
+	}
 }
